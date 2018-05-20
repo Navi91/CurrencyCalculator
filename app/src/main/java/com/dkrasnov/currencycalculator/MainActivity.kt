8d56fc92bf16c8fffc361bf02e375c7905223565
Navi91 <navi91@inbox.ru>
2018-05-20 13:59:04 +0300

    Initial commit.

---
+++ app/src/main/java/com/dkrasnov/currencycalculator/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.dkrasnov.currencycalculator
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.a_main)
+    }
+}
